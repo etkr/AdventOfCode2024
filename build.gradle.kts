d1dae18527fb7fe993c17bada2533c49df2e7e89
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2024-11-30 21:41:42 +0100

    report test results

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation(kotlin("test"))
+    testRuntimeOnly("org.junit.platform:junit-platform-reporting")
+
 }
 
 tasks.test {
@@ -19,4 +21,14 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.withType<Test>().configureEach {
+    val outputDir = reports.junitXml.outputLocation
+    jvmArgumentProviders += CommandLineArgumentProvider {
+        listOf(
+            "-Djunit.platform.reporting.open.xml.enabled=true",
+            "-Djunit.platform.reporting.output.dir=${outputDir.get().asFile.absolutePath}"
+        )
+    }
 }
