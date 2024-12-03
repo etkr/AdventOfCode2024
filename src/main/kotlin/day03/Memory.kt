a9f2ac1ccc890f1c2f033e0ff4a2cd4acf3c4503
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2024-12-04 00:01:32 +0100

    refactor

--- src/main/kotlin/day03/Memory.kt
+++ src/main/kotlin/day03/Memory.kt
@@ -2,7 +2,6 @@ package day03
 
 class Memory(private val memory: String) {
 
-    // Algebraic data types in kotlin !!!
     interface Operation
     object Do : Operation;
     object Dont : Operation;
@@ -10,45 +9,29 @@ class Memory(private val memory: String) {
 
     private val regex = Regex("""(do|don't|mul)\((\d*)?,?(\d*)?\)""")
 
-    private fun matchOperator(op: String, a: String, b: String) = when (op) {
-        "do" -> Do
-        "don't" -> Dont
-        "mul" -> Mul(a.toInt(), b.toInt())
-        else -> throw RuntimeException()
-    }
-
     private fun parseOperation(match: MatchResult) = with(match.destructured) {
-        matchOperator(component1(), component2(), component3())
+        when (component1()) {
+            "do" -> Do
+            "don't" -> Dont
+            "mul" -> Mul(component2().toInt(), component3().toInt())
+            else -> throw RuntimeException()
+        }
     }
 
-    /**
-     * Produce a sequence of operations from the input
-     */
     fun produceOperations() = regex.findAll(memory).map(::parseOperation)
 
-    /**
-     * Part 1
-     */
+    /* Part 1 */
     fun sumOfCleanMemory() = produceOperations().filterIsInstance<Mul>().sumOf { it.a * it.b }
 
-    /**
-     * Pattern matching !!! and tail recursion in kotlin !!!
-     *
-     * Reducer function using tail recursion
-     * - when operation is do, set enabled to true, when don't set enabled to false
-     * - add multiplication operation result if enabled is set.
-     */
-    private tailrec fun sumOperations(list: Sequence<Operation>, accumulator: Int = 0, enabled: Boolean = true): Int =
-        when (val operation = list.firstOrNull()) {
-            null -> accumulator
-            Do -> sumOperations(list.drop(1), accumulator, enabled = true)
-            Dont -> sumOperations(list.drop(1), accumulator, enabled = false)
-            is Mul if (enabled) -> sumOperations(list.drop(1), accumulator + (operation.a * operation.b), enabled)
-            else -> sumOperations(list.drop(1), accumulator, enabled)
-        }
+    data class Accumulator(val value: Int = 0, val enabled: Boolean = true)
 
-    /**
-     * Part 2
-     */
-    fun sumOfCleanMemoryWithDoInstructions() = sumOperations(produceOperations())
+    private fun reducer(accumulator: Accumulator, operation: Operation) = when (operation) {
+        Do -> Accumulator(accumulator.value, true)
+        Dont -> Accumulator(accumulator.value, false)
+        is Mul if (accumulator.enabled) -> Accumulator(accumulator.value + operation.a * operation.b, accumulator.enabled)
+        else -> accumulator
+    }
+
+    /* Part 2 */
+    fun sumOfCleanMemoryWithDoInstructions() = produceOperations().fold(Accumulator(), ::reducer).value
 }
